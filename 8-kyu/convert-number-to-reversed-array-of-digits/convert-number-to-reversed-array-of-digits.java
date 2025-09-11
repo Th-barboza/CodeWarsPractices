c0a5dcb6781ac637017a323db49dff6a8af51529
Thiago B. S. <thiago00.barbosa.silva@gmail.com>
2025-09-11 09:26:28 -0300

    Add solution - CodeHub

---
+++ 8-kyu/convert-number-to-reversed-array-of-digits/convert-number-to-reversed-array-of-digits.java
@@ -0,0 +1,27 @@
+public class Kata {
+  public static int[] digitize(long n) {
+    int i = 1;
+    int j = 0;
+    long ntemp = n;
+    
+    if(n == 0) return new int[]{0};
+    
+    while(true){
+      ntemp /= 10;
+      if(ntemp >= 10) i++;
+      if(ntemp <10) break;
+    }
+    i++;
+    
+    int[] array = new int[i];
+    
+    while(true){
+      if(n<10) break;
+      array[j] = (int) (n%10);
+        n = n/10;
+      j++;
+    }
+    array[j] = (int) n;
+    return array;
+  }
+}
